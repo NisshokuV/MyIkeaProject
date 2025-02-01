777816564710bb38c8c5c859fc8d41873d681035
ryukstark <manuprosperi04@gmail.com>
2025-02-01 17:54:12 +0000

    listo

--- Mylkea/src/main/java/es/cifpcm/Mylkea/controllers/ProductofferController.java
+++ Mylkea/src/main/java/es/cifpcm/Mylkea/controllers/ProductofferController.java
@@ -5,12 +5,19 @@ import es.cifpcm.Mylkea.services.MunicipioService;
 import es.cifpcm.Mylkea.services.ProductofferService;
 import es.cifpcm.Mylkea.services.ProvinciaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class ProductofferController
